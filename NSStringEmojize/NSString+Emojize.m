2e72c147c5fe7e935422a28fe3d1e767f2b13845
Jerome Heymonet <jerome.heymonet@symphony.com>
2019-03-20 15:10:11 +0100

    Add url detector to check if there is an emoji into an URL (linked-in URL)

--- NSStringEmojize/NSString+Emojize.m
+++ NSStringEmojize/NSString+Emojize.m
@@ -20,14 +20,20 @@
 {
     static dispatch_once_t onceToken;
     static NSRegularExpression *regex = nil;
+    static dispatch_once_t dDetector = 0;
+    static NSDataDetector *urlDetector = nil;
     NSMutableArray *matchingRanges = [NSMutableArray new];
     NSMutableArray *matchingLengthChanges = [NSMutableArray new];
     dispatch_once(&onceToken, ^{
         regex = [[NSRegularExpression alloc] initWithPattern:@"(:[a-z0-9-+_]+:)" options:NSRegularExpressionCaseInsensitive error:NULL];
     });
     
+    dispatch_once(&dDetector, ^{
+        urlDetector = [[NSDataDetector alloc] initWithTypes:NSTextCheckingTypeLink error:nil];
+    });
     __block NSString *resultText = text;
     NSRange matchingRange = NSMakeRange(0, [resultText length]);
+    NSArray<NSTextCheckingResult *> *urlMatches = [urlDetector matchesInString:text options:NSMatchingReportCompletion range:matchingRange];
     [regex enumerateMatchesInString:resultText options:NSMatchingReportCompletion range:matchingRange usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
         if ( result &&
             ([result resultType] == NSTextCheckingTypeRegularExpression) &&
@@ -35,10 +41,16 @@
             
             NSRange range = result.range;
             if (range.location != NSNotFound) {
+                NSRange intersectionRange = NSMakeRange(0, 0);
+                for (NSTextCheckingResult *urlMatch in urlMatches) {
+                    intersectionRange = NSIntersectionRange(urlMatch.range, range);
+                    if (intersectionRange.length <= 0) {
+                        break;
+                    }
+                }
                 NSString *code = [text substringWithRange:range];
                 NSString *unicode = self.emojiAliases[code];
-                if (unicode)
-                {
+                if (unicode && !intersectionRange.length > 0) {
                     resultText = [resultText stringByReplacingOccurrencesOfString:code withString:unicode];
                     [matchingRanges addObject:[NSValue valueWithRange: range]];
                     //range.length with be the number of characters reduced
